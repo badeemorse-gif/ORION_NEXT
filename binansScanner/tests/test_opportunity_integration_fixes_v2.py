69456ec96c04d5f0b5c4665bfd49664ef6a73b0f
badeemorse-gif <badeemorse@gmail.com>
2026-08-14 03:15:44 +0300

    chore(opportunity): remove temporary integration probe

--- binansScanner/tests/test_opportunity_integration_fixes_v2.py
+++
@@ -1,5 +0,0 @@
-import unittest
-from engines.opportunity_intelligence import OpportunityCandidateGenerator
-class TestOpportunityIntegrationFixesV2(unittest.TestCase):
-    def test_generator_exists(self): self.assertTrue(callable(OpportunityCandidateGenerator.generate))
-if __name__ == "__main__": unittest.main()
