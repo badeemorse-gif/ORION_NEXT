e8887f67c8a972f77178b8e08782c2ea9d797d90
badeemorse-gif <badeemorse@gmail.com>
2026-09-02 20:55:53 +0300

    Export replay runner and audit APIs

--- binansScanner/replay/__init__.py
+++ binansScanner/replay/__init__.py
@@ -8,8 +8,10 @@ _BINANS_SCANNER = _REPO_ROOT / "binansScanner"
 if str(_BINANS_SCANNER) not in sys.path:
     sys.path.insert(0, str(_BINANS_SCANNER))
 
+from replay.audit import MovementAudit, build_movement_audit, correlate_opportunity_refreshes
 from replay.clock import ReplayClock
 from replay.dataset import HistoricalDataset, HistoricalDatasetManifest, HistoricalMarketEvent
+from replay.runner import HistoricalPaperReplayRunner, ReplayConfig
 from replay.source import HistoricalMarketDataSource
 from replay.stream import HistoricalMarketEventStream
 
@@ -19,5 +21,10 @@ __all__ = [
     "HistoricalMarketEvent",
     "HistoricalMarketDataSource",
     "HistoricalMarketEventStream",
+    "HistoricalPaperReplayRunner",
     "ReplayClock",
+    "ReplayConfig",
+    "MovementAudit",
+    "build_movement_audit",
+    "correlate_opportunity_refreshes",
 ]
